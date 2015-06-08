b80d1bb3124d3825ee66fa4c88ad6e33191961f2
Daniel Mendez <albert.dnl.md@gmail.com>
2015-06-08 10:58:01 -0600

    Script base de Datos

---
+++ script/dbHallSrum.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE Usuario(
+	idUsuario SERIAL,
+	nombre VARCHAR(255),
+	apellido VARCHAR(255),
+	nickname VARCHAR(255),
+	contrasena VARCHAR(255)
+	
+);
+
+INSERT INTO Usuario(nombre,apellido,nickname,contrasena) VALUES('Daniel','Mendez','rose','123');
+
+SELECT * from Usuario;
